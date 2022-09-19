6be7b286ea457a596ec8690a6b2b69018685f115
john <2236313680@qq.com>
2022-09-20 00:04:18 +0800

    style: merge

--- components/IndexComponents/WorkItem.vue
+++ components/IndexComponents/WorkItem.vue
@@ -1,191 +1,176 @@
 <template>
-  <uni-swipe-action>
-    <uni-swipe-action-item :left-options="todoSlideBlockRightOptions" @click="clickSlide">
-      <view class="workItem-wrapper" :class="[data.grade]">
-        <fui-row margin-bottom="24rpx">
-          <fui-col :span="1">
-            <view class="workItem-wrapper-left"></view>
-          </fui-col>
-          <fui-col :span="23">
-            <view class="workItem-wrapper-top">
-              <MyTag v-for="item in data.tag" :key="item" :title="item"></MyTag>
-              <view class="workItem-wrapper-top-dropdown">
-                <DropDownCom :data="dropDownOptions" :id="data.id">
-                  <fui-icon name="more-fill" class="workItem-wrapper-top-icon"></fui-icon>
-                </DropDownCom>
-              </view>
-            </view>
-            <view class="workItem-wrapper-right" @click="detail(data.id)">
-              <view class="workItem-wrapper-right">
-                <view class="title">
-                  <h1>{{ data.title }}</h1>
-                </view>
-                <view class="desc">{{ data.desc }}</view>
-                <MyDate :ddl="data.ddl"></MyDate>
-                <!-- https://s1.ax1x.com/2022/09/17/xpMRZ6.png -->
-                <!-- https://s1.ax1x.com/2022/09/17/xpSLyd.png -->
-                <img src="https://s1.ax1x.com/2022/09/17/xpMRZ6.png" alt="截止" class="image">
-              </view>
-            </view>
-          </fui-col>
-        </fui-row>
-      </view>
-    </uni-swipe-action-item>
-  </uni-swipe-action>
+	<uni-swipe-action>
+		<uni-swipe-action-item :left-options="todoSlideBlockRightOptions" @click="clickSlide">
+			<view class="workItem-wrapper" :class="[data.grade]">
+				<fui-row margin-bottom="24rpx">
+					<fui-col :span="1">
+						<view class="workItem-wrapper-left"></view>
+					</fui-col>
+					<fui-col :span="23">
+						<view class="workItem-wrapper-top">
+							<MyBadge v-for="item in data.tag" :key="item" :txt="item" isread="true"></MyBadge>
+							<view class="workItem-wrapper-top-dropdown">
+								<DropDownCom :data="dropDownOptions" :id="data.id">
+									<fui-icon name="more-fill" class="workItem-wrapper-top-icon"></fui-icon>
+								</DropDownCom>
+							</view>
+						</view>
+						<view class="workItem-wrapper-right" @click="detail(data.id)">
+							<view class="title">
+								<h1>{{ data.title }}</h1>
+							</view>
+							<view class="desc">{{ data.desc }}</view>
+							<MyDate :ddl="data.ddl"></MyDate>
+							<!-- https://s1.ax1x.com/2022/09/17/xpMRZ6.png -->
+							<!-- https://s1.ax1x.com/2022/09/17/xpSLyd.png -->
+							<img src="https://s1.ax1x.com/2022/09/17/xpMRZ6.png" alt="截止" class="image">
+						</view>
+					</fui-col>
+				</fui-row>
+			</view>
+		</uni-swipe-action-item>
+	</uni-swipe-action>
 </template>
 
 <script setup>
-  import MyTag from "@/components/shared/MyTag/index.vue"
-  import MyDate from "@/components/shared/MyDate/index.vue"
-  import DropDownCom from "@/components/shared/DropDown/index.vue"
-  import mod from "./module.js"
+	import MyDate from "@/components/shared/MyDate/index.vue"
+	import MyBadge from "@/components/shared/MyBadge/index.vue"
+	import DropDownCom from "@/components/shared/DropDown/index.vue"
+	import mod from "./module.js"
 
-  const {
-    router,
-    defineProps,
-    ref,
-    provide,
-    todoSlideBlockRightOptions,
-    enumSlideBlockOptionsEnum,
-    checkSubscribe,
-    onHide
-  } = mod
+	const {
+		router,
+		defineProps,
+		ref,
+		provide,
+		todoSlideBlockRightOptions,
+		enumSlideBlockOptionsEnum,
+		checkSubscribe,
+		onHide
+	} = mod
 
-  const props = defineProps({
-    data: {
-      type: Object,
-      default: {
-        id: 1,
-        time: "2022-09-05 13:34",
-        title: "数据库概论-456班",
-        desc: "这是一段简短的描述，这是一段简短的描述",
-        tag: ["作业二"],
-        ddl: "2022-09-06 13:34",
-        grade: "danger"
-      }
-    },
-    dropDownOptions: {
-      type: Array,
-      default: []
-    }
-  })
+	const props = defineProps({
+		data: {
+			type: Object,
+			default: {
+				id: 1,
+				time: "2022-09-05 13:34",
+				title: "数据库概论-456班",
+				desc: "这是一段简短的描述，这是一段简短的描述",
+				tag: ["作业二"],
+				ddl: "2022-09-06 13:34",
+				grade: "danger"
+			}
+		},
+		dropDownOptions: {
+			type: Array,
+			default: []
+		}
+	})
 
 
-  const detail = (id) => {
-    // 如果已经设置订阅就进行发起请求
-    checkSubscribe(uni)
-    // 跳转路由
-    router(uni, {
-      url: "detail",
-      params: {
-        id,
-      },
-    })
-  }
+	const detail = (id) => {
+		// 如果已经设置订阅就进行发起请求
+		checkSubscribe(uni)
+		// 跳转路由
+		router(uni, {
+			url: "detail",
+			params: {
+				id,
+			},
+		})
+	}
 
-  const clickSlide = (e) => {
-    const {
-      content
-    } = e;
-    if (content.text === enumSlideBlockOptionsEnum.FIRST_BUTTON) {
-      console.log("点击了完成");
-      // TODO: 完成逻辑实现
-    }
-  }
+	const clickSlide = (e) => {
+		const {
+			content
+		} = e;
+		if (content.text === enumSlideBlockOptionsEnum.FIRST_BUTTON) {
+			console.log("点击了完成");
+			// TODO: 完成逻辑实现
+		}
+	}
 
-  // 控制三个点的开闭
-  let flag = ref(false);
-  provide("flag", flag)
+	// 控制三个点的开闭
+	let flag = ref(false);
+	provide("flag", flag)
 
-  // 处理三个点闭合的善后
-  onHide(() => {
-    if (flag.value) {
-      flag.value = false
-    }
-  })
+	// 处理三个点闭合的善后
+	onHide(() => {
+		if (flag.value) {
+			flag.value = false
+		}
+	})
 </script>
 
 <style lang="scss" scoped>
-  .workItem-wrapper {
-    box-sizing: border-box;
-    // width: 95%;
-    height: 320rpx;
-    // padding: 6px;
-    border-radius: 0px 10px 10px 0px;
-    background-color: whitesmoke;
-    border-bottom: 2px dashed gainsboro;
-    // box-shadow:  6px 6px 12px 12px #1e1e1e;
-    box-shadow: 0px 0px 15px #1e1e1e;
-    margin: 0px 3px 0px 0px;
+	.workItem-wrapper {
+		position: relative;
+		overflow: hidden;
+		box-sizing: border-box;
+		// width: 95%;
+		height: 300rpx;
+		// padding: 6px;
+		border-radius: 0px 10px 10px 0px;
+		background-color: whitesmoke;
+		border-bottom: 2px dashed gainsboro;
+		// box-shadow:  6px 6px 12px 12px #1e1e1e;
+		box-shadow: 0px 0px 15px #1e1e1e;
+		margin: 0px 4px 0px 0px;
 
-    &-left {
-      background-color: seagreen;
-      width: 5px;
-      height: 320rpx;
-    }
+		&-left {
+			background-color: seagreen;
+			width: 6px;
+			height: 300rpx;
+		}
 
-    &-right {
-      margin: 0px 5px;
+		&-right {
+			margin: 0px 15px;
 
-      .title {
-        height: 70rpx;
-        font-size: 50rpx;
-        font-weight: bold;
-        margin: 8px 0px;
-        overflow: hidden;
-        // text-overflow: ellipsis;
-        // white-space: nowrap;
-      }
+			.title {
+				height: 60rpx;
+				font-size: 43rpx;
+				font-weight: bold;
+				margin: 5px 0px;
+				overflow: hidden;
+				// text-overflow: ellipsis;
+				// white-space: nowrap;
+			}
 
-      .desc {
-        color: gray;
-        font-size: 30rpx;
-        width: 80%;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-      }
+			.desc {
+				color: gray;
+				font-size: 30rpx;
+				width: 80%;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				white-space: nowrap;
+			}
 
-      .image {
-        position: absolute;
-        top: 75px;
-        right: -10px;
-        width: 100px;
-        height: 100px;
-      }
+			.image {
+				position: absolute;
+				// top: 75px;
+				// right: -10px;
+				top: 150rpx;
+				right: -40rpx;
+				width: 100px;
+				height: 100px;
 
-      .date {
-        display: flex;
-        justify-content: left;
-        align-items: center;
-        margin-top: 5px;
+			}
 
-        .date-icon {
-          width: 25px;
-          height: 25px;
-        }
+		}
 
-        .data-ddl {
-          color: darkblue;
-          font-weight: bold;
-          font-size: 30rpx;
-        }
+		&-top {
+			margin-top: 5px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
 
-      }
-    }
-
-    &-top {
-      margin-top: 5px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-
-      &-dropdown {
-        position: absolute;
-        z-index: 100;
-        width: 100px;
-        right: 10rpx;
-      }
-    }
-  }
+			&-dropdown {
+				position: absolute;
+				z-index: 100;
+				width: 100px;
+				right: 10rpx;
+			}
+		}
+	}
 </style>
--- pages/detail/index.vue
+++ pages/detail/index.vue
@@ -1,122 +1,119 @@
 <script setup>
-  import mod from "./module.js"
-  import MyDate from "@/components/shared/MyDate/index.vue"
-  import MyTag from "@/components/shared/MyTag/index.vue"
-  const {
-    onLoad,
-    reactive,
-    defineProps,
-    requestData
-  } = mod
-  // 预览图片
-  const previewImg = (url) => {
-    uni.previewImage({
-      urls: [url],
-    });
-  }
-  let data = reactive({})
-  onLoad(async (options) => {
-    const {
-      id,
-    } = options
-    // 传值不对，或未传值
-    if (!id) {
-      return uni.navigateBack({
-        delta: 1
-      })
-    }
-    Object.assign(data, await requestData(uni, {}, true))
-  })
+	import mod from "./module.js"
+	import MyDate from "@/components/shared/MyDate/index.vue"
+	import MyTag from "@/components/shared/MyTag/index.vue"
+	const {
+		onLoad,
+		reactive,
+		defineProps,
+		requestData
+	} = mod
+	// 预览图片
+	const previewImg = (url) => {
+		uni.previewImage({
+			urls: [url],
+		});
+	}
+	let data = reactive({})
+	onLoad(async (options) => {
+		const {
+			id,
+		} = options
+		// 传值不对，或未传值
+		if (!id) {
+			return uni.navigateBack({
+				delta: 1
+			})
+		}
+		Object.assign(data, await requestData(uni, {}, true))
+	})
 </script>
 
 <template>
-  <view class="detail-wrapper">
-    <view class="detail-wrapper-lining">
-      <view class="detail-wrapper-lining-time">
-        发布于: {{ data.time }}
-      </view>
-      <header class="detail-wrapper-lining-title">
-        {{ data.title }}
-      </header>
-      <main class="detail-wrapper-lining-desc">
-        {{ data.desc }}
-      </main>
-      <view class="detail-wrapper-lining-urls">
-        <view v-for="item in data.urls" :key="item" class="detail-wrapper-lining-urls-item">
-          <img :src="item" @click="previewImg(item)">
-        </view>
-      </view>
-      <view class="detail-wrapper-lining-tag">
-        <MyTag v-for="item in data.tag" :key="item" :title="item"></MyTag>
-      </view>
-      <MyDate :ddl="data.ddl"></MyDate>
-    </view>
-  </view>
+	<view class="detail-wrapper">
+		<view class="detail-wrapper-lining">
+			<view class="detail-wrapper-lining-time">
+				发布于: {{ data.time }}
+			</view>
+			<header class="detail-wrapper-lining-title">
+				{{ data.title }}
+			</header>
+			<main class="detail-wrapper-lining-desc">
+				{{ data.desc }}
+			</main>
+			<view class="detail-wrapper-lining-urls">
+				<view v-for="item in data.urls" :key="item" class="detail-wrapper-lining-urls-item">
+					<img :src="item" @click="previewImg(item)">
+				</view>
+			</view>
+			<view class="detail-wrapper-lining-tag">
+				<MyTag v-for="item in data.tag" :key="item" :title="item"></MyTag>
+			</view>
+			<MyDate :ddl="data.ddl"></MyDate>
+		</view>
+	</view>
 </template>
 
 <style lang="scss" scoped>
-  .workItem-wrapper {
-    position: relative;
-    overflow: hidden;
-    box-sizing: border-box;
-    // width: 95%;
-    height: 300rpx;
-    // padding: 6px;
-    border-radius: 0px 10px 10px 0px;
-    background-color: whitesmoke;
-    border-bottom: 2px dashed gainsboro;
-    // box-shadow:  6px 6px 12px 12px #1e1e1e;
-    box-shadow: 0px 0px 15px #1e1e1e;
-    margin: 0px 4px 0px 0px;
+	.detail-wrapper {
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		width: 100vw;
+		height: 100vh;
+		padding: 40rpx 0;
+		background-color: #f0f0f0;
 
-    &-left {
-      background-color: seagreen;
-      width: 6px;
-      height: 300rpx;
-    }
+		&-lining {
+			width: 90%;
+			margin: 0 auto;
 
-    &-right {
-      margin: 0px 15px;
+			&-time {
+				font-size: 28rpx;
+				color: #938e8e;
+			}
 
-      .title {
-        height: 60rpx;
-        font-size: 43rpx;
-        font-weight: bold;
-        margin: 5px 0px;
-        overflow: hidden;
-        // text-overflow: ellipsis;
-        // white-space: nowrap;
-      }
+			&-title {
+				font-size: 50rpx;
+				font-weight: 700;
+				margin-top: 10rpx;
+			}
 
-      .desc {
-        color: gray;
-        font-size: 30rpx;
-        width: 80%;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-      }
+			&-desc {
+				width: 95%;
+				margin-top: 10rpx;
 
-      .image {
-        position: absolute;
-        // top: 75px;
-        // right: -10px;
-        top: 150rpx;
-        right: -40rpx;
-        width: 100px;
-        height: 100px;
-      }
-    }
+				font-size: 30rpx;
+				line-height: 36rpx;
+			}
 
-    &-top {
-      margin-top: 5px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+			&-urls {
+				display: flex;
+				justify-content: space-between;
+				flex-flow: wrap;
+				align-items: center;
+				width: 100%;
+				margin-top: 10px;
 
-      &-icon {
-        padding-right: 10px;
-      }
-    }
-  }
+				&-item {
+					width: 30%;
+
+					image {
+						width: 100%;
+						height: 100px;
+						border-radius: 10rpx;
+					}
+				}
+			}
+
+			&-tag {
+				display: flex;
+				align-items: center;
+				flex-flow: wrap;
+
+			}
+		
+			
+		}
+	}
 </style>
